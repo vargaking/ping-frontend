758539efd81e0b23838e3a7b668237d60130d96d
Dani Gergely <daniel.gergely.2005@gmail.com>
2025-12-26 15:40:19 +0100

    load proxy ip from .env

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,22 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), basicSsl()],
-    server: {
-        proxy: {
-            '/api': 'http://192.168.1.249:8000',
-            '/auth': 'http://192.168.1.249:8000',
-            '/users': 'http://192.168.1.249:8000',
-            '/servers': 'http://192.168.1.249:8000',
-            '/channels': 'http://192.168.1.249:8000',
-            '/ws': {
-                target: 'ws://192.168.1.249:8000',
-                ws: true
-            }
-        }
-    }
+	server: {
+		proxy: {
+			'/api': `http://${process.env.LOCAL_IP}:8000`,
+			'/auth': `http://${process.env.LOCAL_IP}:8000`,
+			'/users': `http://${process.env.LOCAL_IP}:8000`,
+			'/servers': `http://${process.env.LOCAL_IP}:8000`,
+			'/channels': `http://${process.env.LOCAL_IP}:8000`,
+			'/ws': {
+				target: `ws://${process.env.LOCAL_IP}:8000`,
+				ws: true
+			}
+		}
+	}
 });
