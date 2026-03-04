b1d77c38ccdf381b0a4df5dad31be9b75365d9a6
Dániel Gergely <daniel.gergely.2005@gmail.com>
2026-03-04 14:39:10 +0100

    change some configs + tailwind typography

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export default defineConfig({
@@ -15,10 +16,7 @@ export default defineConfig({
 			'/servers': `http://${process.env.LOCAL_IP}:8000`,
 			'/channels': `http://${process.env.LOCAL_IP}:8000`,
 			'/invites': `http://${process.env.LOCAL_IP}:8000`,
-			'/ws': {
-				target: `ws://${process.env.LOCAL_IP}:8000`,
-				ws: true
-			}
+			'/ws': { target: `ws://${process.env.LOCAL_IP}:8000`, ws: true }
 		}
 	}
 });
